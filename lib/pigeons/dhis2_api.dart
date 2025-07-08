982b7f7ee70afaf9b9723e9f29e0633f551af1d7
yuzalsif <yuseifabdillah@gmail.com>
2025-07-08 15:06:25 +0300

    feat: Return LoginResult instead of a String for login func

---
+++ lib/pigeons/dhis2_api.dart
@@ -0,0 +1,40 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/generated/dhis2_api.g.dart',
+  kotlinOut:
+  'android/app/src/main/kotlin/com/example/dhis2_flutter_test_impl/Dhis2Api.g.kt',
+  kotlinOptions: KotlinOptions(
+    package: 'com.example.dhis2_flutter_test_impl',
+    errorClassName: 'Dhis2PigeonError',
+  ),
+))
+
+class LoginCredentials {
+  LoginCredentials({
+    required this.serverUrl,
+    required this.username,
+    required this.password,
+  });
+  final String serverUrl;
+  final String username;
+  final String password;
+}
+
+class LoginResult {
+  LoginResult({required this.username, required this.successMessage});
+  final String username;
+  final String successMessage;
+}
+
+@HostApi()
+abstract class Dhis2LoginApi {
+  @async
+  LoginResult login(LoginCredentials credentials);
+
+  @async
+  bool isUserLoggedIn();
+
+  @async
+  void logout();
+}
